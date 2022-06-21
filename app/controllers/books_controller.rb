1c9320a485d699bcd8eb3bbfcc95ed112911028d
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-06-21 02:18:18 +0000

    [Finish]復習編:課題3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,13 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
   def show
     @book = Book.find(params[:id])
-    @user = @book.user
-    @newbook = Book.new
     @book_comment = BookComment.new
   end
 
   def index
-    @user = current_user
     @books = Book.all
     @book = Book.new
   end
@@ -19,27 +19,18 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
-      @user = current_user
       render 'index'
     end
   end
 
   def edit
-    @book = Book.find(params[:id])
-    if @book.user == current_user
-      render "edit"
-    else
-      redirect_to books_path
-    end
   end
 
   def update
-    @book = Book.find(params[:id])
-    @book.user_id = current_user.id
     if @book.update(book_params)
-      redirect_to book_path(@book.id), notice: "You have updated book successfully."
+      redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render :edit
+      render "edit"
     end
   end
 
@@ -55,5 +46,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
 end
