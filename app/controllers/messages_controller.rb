c9dc25727d5fe1a09071ce344036d98630dae5e9
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-07-04 09:20:44 +0000

    [Update]ポートフォリオ応用編:8a参考コードへ修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,38 @@
 class MessagesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :reject_non_related, only: [:show]
+
+  def show
+    @user = User.find(params[:id])
+    rooms = current_user.entries.pluck(:room_id)
+    entries = Entry.find_by(user_id: @user.id, room_id: rooms)
+
+    unless entries.nil?
+      @room = entries.room
+    else
+      @room = Room.new
+      @room.save
+      Entry.create(user_id: current_user.id, room_id: @room.id)
+      Entry.create(user_id: @user.id, room_id: @room.id)
+    end
+    @messages = @room.messages
+    @message = Message.new(room_id: @room.id)
+  end
 
   def create
-    message = Message.new(message_params)
-    message.user_id = current_user.id
-    if message.save
-      redirect_to room_path(message.room)
-    else
-      redirect_back(fallback_location: root_path)
-    end
+    @message = current_user.messages.new(message_params)
+    render :validater unless @message.save
   end
 
   private
 
     def message_params
-      params.require(:message).permit(:room_id, :body)
+      params.require(:message).permit(:message, :room_id)
     end
+
+  def reject_non_related
+    user = User.find(params[:id])
+    unless current_user.following?(user) && user.following?(current_user)
+      redirect_to books_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'relationships/followings'
-  get 'relationships/followers'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "homes#top"
   get "/home/about"=>"homes#about"
+
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
@@ -17,9 +15,9 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
 
-  resources :messages, only: [:create]
-  resources :rooms, only: [:create, :index, :show]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "search" => "searches#search"
 
+  resources :messages, only: [:show, :create]
 end
