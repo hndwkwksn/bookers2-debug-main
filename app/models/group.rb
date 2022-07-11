33920cfea0cb425a525fbcd431651b438b030b3f
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-07-11 23:59:59 +0900

    [Add]ポートフォリオ応用編:7c/group.rb/image投稿機能追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,9 @@
 class Group < ApplicationRecord
   has_many :group_users, dependent: :destroy, foreign_key: 'group_id'
   has_many :users, through: :group_users
+  has_one_attached :image
+
+  def get_image
+    (image.attached?) ? image : 'no_image.jpg'
+  end
 end
