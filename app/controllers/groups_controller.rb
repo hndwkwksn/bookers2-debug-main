41d382c8a86d4069de7792b136ff482d36469194
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-07-30 13:22:18 +0000

    [Add]新規投稿/編集機能/※ストロングパラメータが上手く機能しない

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,12 +2,39 @@ class GroupsController < ApplicationController
   def new
   end
 
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to groups_path
+    else
+      render 'new'
+    end
+  end
+
   def index
+    @groups = Group.all
   end
 
   def show
+    @group = Group.find(params[:id])
   end
 
   def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+      if @group.update(group_params)
+        redirect_to group_path(@group.id)
+      else
+        render 'edit'
+      end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:image, :name, :introduction)
   end
 end
