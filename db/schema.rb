5b354774cc6d12bf60fea532650f0e9aaf22f668
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-09-01 13:44:16 +0000

    [Update]massege機能/修正

--- db/schema.rb
+++ db/schema.rb
@@ -98,7 +98,7 @@ ActiveRecord::Schema.define(version: 2022_07_10_235340) do
   create_table "messages", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "room_id", null: false
-    t.text "body"
+    t.text "message", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["room_id"], name: "index_messages_on_room_id"
