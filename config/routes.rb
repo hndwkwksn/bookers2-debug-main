72f944712d14c9b1f0f416fe06898b02139d30e7
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-09-02 10:36:50 +0000

    [Add]groups_controller/destroy/join

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,9 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
 
-  resources :groups, only: [:new,:index,:show,:create,:edit,:update]
+  resources :groups, only: [:new,:index,:show,:create,:edit,:update,:destroy] do
+    get "join" => "groups#join"
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
