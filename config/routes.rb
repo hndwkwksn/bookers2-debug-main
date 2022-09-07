bb22033f2934dd3adb2c66bf777445f93b1538a6
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-09-07 03:22:46 +0000

    [Add]投稿記録機能/日付指定検索/config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
+    get "search", to: "users#search"
   end
 
   resources :groups, only: [:new,:index,:show,:create,:edit,:update,:destroy] do
